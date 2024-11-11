909c5b05ae7cf37d274be8ed4182785a4c5fa699
AusteId <auste.idaite@gmail.com>
2024-11-11 07:56:33 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+
+  eslintConfigPrettier,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "prefer-const": [
+        "error",
+        {
+          destructuring: "any",
+          ignoreReadBeforeAssign: false,
+        },
+      ],
+    },
+  },
+
+];
